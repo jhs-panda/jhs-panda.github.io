a987304ad9d6334136d10520470278d0c1a0030b
jhs-panda <jhs-panda>
2022-08-13 12:57:26 -0400

    Final Draft 8/13

---
+++ joy_song/private.js
@@ -0,0 +1,13 @@
+let submitButton = document.querySelector('#submit');
+let content = document.querySelector("#privacy");
+const correctPassword="1234";
+function clickListener(event){
+    event.preventDefault();
+    let input_password=document.querySelector('#password');
+    let password_value=input_password.value;
+    if(password_value===correctPassword){
+        console.log("Access granted!");
+        content.classList.remove("hidden");
+    }
+}
+submitButton.addEventListener('click', clickListener);
